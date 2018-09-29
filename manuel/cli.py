66e012b8bf8ca33bba801ed200af547c63ddaa74
Xevi <xbarnada@gmail.com>
2018-09-29 11:01:08 +0200

    Add parameter to recreate materialized view

--- manuel/cli.py
+++ manuel/cli.py
@@ -9,23 +9,32 @@ def manuel():
     pass
 
 
-def invoke():
-    manuel()
-
-
 @manuel.command()
 @click.argument('config_file')
 @click.option('--index/--no-index', default=False)
+@click.option('--recreate/--no-recreate', default=False)
 @click.option('--debug/--no-debug', default=False)
-def cli_generate_report(config_file, index, debug):
+def cli_generate_report(config_file, index, recreate, debug):
     """
     CLI entry point
 
     :param config_file:
     :param index:
-    :param debug:
+    :param recreate: Recreates the materialized view
+    :type recreate: bool
+    :param debug:Enables debug mode
+    :type debug: bool
     :return:
     """
     if index:
         create_index(config_file, debug)
+    if recreate:
+        generate_materialized_vies(config_file, debug)
     generate_report(config_file, debug)
+
+
+
+
+
+def invoke():
+    manuel()
