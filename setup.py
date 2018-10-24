1a33ae30820e4617bfccfca653cab26369cee3d1
Xevi <xbarnada@gmail.com>
2018-10-24 20:55:07 +0200

    Bump v0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = data.split()
 
 setup(
     name='manuel',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     url='https://github.com/Xevib/manuel',
     license='MIT',
