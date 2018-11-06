2ecc49c05c84bd7ed8610c7c20677dec317e38c5
Xevi <xbarnada@gmail.com>
2018-11-06 20:22:14 +0100

    Bump to v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = data.split()
 
 setup(
     name='manuel',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     url='https://github.com/Xevib/manuel',
     license='MIT',
